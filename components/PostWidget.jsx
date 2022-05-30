2089f5c0c574bc21e9b7c7950e0c711e6e8ad853
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-05-30 13:56:50 -0600

    services similar posts query +++

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import moment from 'moment';
 import Link from 'next/link';
 
+import { getRecentPosts } from '../services';
+
+const PostWidget = ({categories, slug}) => {
+
+    const [relatedPosts, setRelatedPosts] = useState([]);
+
+    useEffect(() => {
+if(slug) {
+    getSimilarPosts(category, slug)
+    .then((result) => setRelatedPosts(result))
+}else {
+    getRecentPosts(category, slug)
+    .then((result) => setRelatedPosts(result))
+}
+    }, [input])
 
 
-const PostWidget = () => {
     return (
         <div>
             PostWidget
--- services/index.js
+++ services/index.js
@@ -62,6 +62,29 @@ export const getRecentPosts = async () => {
 }
 
 
+export const getSimilarPosts = async () => {
+  const query = gql `
+  query getPostDetails($slug: String!, $categories: [String!]) {
+    posts(
+      where: {slug_not: $slug, AND {categories_some: {slug_in: $categories}}}
+      last:3
+    ) {
+      title
+      featuredImage {
+        url
+      }
+      createdAt
+      slug
+    }
+  }
+  `
+
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
+
 
 
 
