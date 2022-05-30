c411f77ac7db02445c0f1bdb9013985b1ada94de
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-05-30 13:42:02 -0600

    postcard styling updates + content

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 import photoUrl from '../public/bg.jpeg'
 
-
+import moment from 'moment';
 
 const PostCard = ({post}) => {
     // console.log(post.author)
@@ -31,11 +31,25 @@ const PostCard = ({post}) => {
                 height='30px'
                 width='30px'
                 className='align-middle rounded-full'
-                // src='./avatar.png'
                 src={post.author.photo.url}
                 />
+                <p className='inline ml-2 text-lg text-gray-700 align-middle'>{post.author.name}</p>
+                </div>
+                <div className='font-medium text-gray-700'>
+                <svg xmlns="http://www.w3.org/2000/svg" className="inline w-6 h-6 mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+        </svg>
+        <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
                 </div>
             </div>
+            <p className='px-4 mb-8 text-lg font-normal text-center text-gray-700 lg:p-2'>{post.excerpt}</p>
+        <div className='text-center'>
+            <Link href={`/post/${post.slug}`}>
+                <span className='inline-block px-8 py-2 text-lg font-medium text-white transition duration-500 transform bg-pink-600 rounded-full cursor-pointer hover:-translate-y-1'>
+                    Continue Reading
+                </span>
+            </Link>
+        </div>
         </div>
     )
 }
