cb38872408936c5866f4e274858f4b8553debda8
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-05-30 15:37:11 -0600

    +

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -7,20 +7,20 @@ import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm} from
 
 
 
-const PostDetails = () => {
+const PostDetails = ( {post} ) => {
     return (
         <div className='container px-10 mx-auto mb-8'>
             <div className='grid grid-cols-1 gap-12 lg:grid-cols-12'>
                 <div className='col-span-1 lg:col-span-8'>
-        <PostDetail/>
-        <Author/>
-        <CommentsForm/>
-        <Comments/>
+        <PostDetail post={post}/>
+        <Author author={post.author}/>
+        <CommentsForm slug={post.slug}/>
+        <Comments slug={post.slug}/>
 
                 </div>
                 <div className='col-span-1 lg:col-span-4'>
                     <div className='relative lg:sticky top-8'>
-                        <PostWidget/>
+                        <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)}/>
                         <Categories/>
                     </div>
                     
@@ -33,11 +33,11 @@ const PostDetails = () => {
 
 export default PostDetails;
 
-export async function getStaticProps() {
+export async function getStaticProps({params}) {
     // return posts or return empty array
-    const posts = (await getPosts()) || [];
+    const data = await getPostDetails(params.slug);
   
   return {
-      props: { posts }
+      props: { post: data}
     }
   }
