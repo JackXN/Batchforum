58a5ce5ce0d423885922e963644754ec65cd16c3
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-05-29 23:37:54 -0600

    photo url null err, fix tmr

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+// COmponents
+import Link from 'next/link';
+
+
 const PostCard = ({post}) => {
-    // console.log(post)
+    // console.log(post.author)
+    console.log(post)
     return (
         <div className='p-0 pb-12 mb-8 bg-white rounded-lg shadow-lg lg:p-8'>
             <div className='relative mb-6 overflow-hidden shadow-md pb-80'>
@@ -10,6 +15,23 @@ const PostCard = ({post}) => {
                 alt={post.title}
                 className='absolute object-cover object-top w-full rounded-t-lg shadow-lg h-80 lg:rounded-lg'
                 />
+            </div>
+            <h1 className='mb-8 text-3xl font-semibold text-center transition duration-100 cursor-pointer hover:text-pink-600'>
+<Link href={`/post/${post.slug}`}>
+    {post.title}
+</Link>
+            </h1>
+            <div className='items-center justify-center block w-full mb-8 text-center lg:flex'>
+                <div className='flex items-center justify-center w-full mb-4 mr-8 lg:mb-0 lg:w-auto'>
+                <img
+                alt={post.author.name}
+                height='30px'
+                width='30px'
+                className='align-middle rounded-full'
+                src={post.author.photo.url}
+                
+                />
+                </div>
             </div>
         </div>
     )
--- services/index.js
+++ services/index.js
@@ -4,10 +4,11 @@ import { request, gql } from 'graphql-request';
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
-  const query = gql`
-query MyQuery {
-        postsConnection {
+    const query = gql`
+      query MyQuery {
+        postConnection {
           edges {
+            cursor
             node {
               author {
                 bio
@@ -17,23 +18,23 @@ query MyQuery {
                   url
                 }
               }
+              createdAt
               slug
               title
               excerpt
-              createdAt
               featuredImage {
                 url
+              }
+              categories {
+                name
+                slug
               }
             }
           }
         }
-        categories {
-          name
-          slug
-        }
       }
-
-`
+    `;
+  
 const result = await request(graphqlAPI, query);
 
 return result.postsConnection.edges;
