7582de612b9457da8f66fd0cc616a416bd279b86
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-05-31 06:36:38 -0600

    PostDetail ++

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import moment from 'moment';
+import parse from 'html-react-parser';
 
-const Comments = () => {
+
+import { getComments } from '../services';
+
+
+const Comments = ({slug}) => {
+
+    const [comments,setComments] = useState([])
+
+    useEffect(() => {
+        getComments(slug).then((result) => {
+            setComments(result)
+        })
+    }, []);
+
+    console.log(comments)
+
+
+
+
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <>
+        {comments.length > 0 && (
+          <div className="p-8 pb-12 mb-8 bg-white rounded-lg shadow-lg">
+            <h3 className="pb-4 mb-8 text-xl font-semibold border-b">
+              {comments.length}
+              {' '}
+              Comments
+            </h3>
+              {comments.map((comment, index) => (
+                <div key={index} className="pb-4 mb-4 border-b border-gray-100">
+                  <p className="mb-4">
+                    <span className="font-semibold">{comment.name}</span>
+                    {' '}
+                    on
+                    {' '}
+                    {moment(comment.createdAt).format('MMM DD, YYYY')}
+                  </p>
+                  <p className="w-full text-gray-600 whitespace-pre-line">{parse(comment.comment)}</p>
+                </div>
+              ))}
+          </div>
+        )}
+      </>
+    );
+  };
 
 export default Comments
--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from 'react';
 
-const PostDetail = () => {
+import moment from 'moment';
+
+
+const PostDetail = ({post}) => {
+
+const getContentFragment = (index, text, obj, type) => {
+    let modifiedText = text;
+
+    if(obj) {
+        if(obj.bold) {
+            modifiedText = (<em key={index}>{text}</em>);
+        }
+        if(obj.italic) {
+            modifiedText = (<em key={index}>{text}</em>)
+        }
+        if(obj.underline) {
+            modifiedText = (<u key={index}>{text}</u>)
+        }
+    }
+
+switch(type) {
+    case 'heading-three': 
+    return <h3 key={index} className='mb-4 text-xl font-semibold'>{modifiedText.map((item,i) => <React.fragment key={i}>{item}</React.fragment>)}</h3>
+    case 'paragraph':
+        return <p key={index} className='mb-8'>{modifiedText.map((item,i) => <React.fragment key={i}>{item}</React.fragment>)}</p>
+        case 'heading-four':
+            return <h4 key={index} className='mb-4 font-semibold text-md'>{modifiedText.map((item, i) => <React.fragment key={i}>{item}</React.fragment>)}</h4>
+            case 'image':
+                return (
+                    <img
+                    key={index}
+                    alt={obj.title}
+                    height={obj.height}
+                    width={obj.width}
+                    src={obj.src}
+                    />
+                )
+                default: 
+                return modifiedText;
+        }
+
+}
+
     return (
-        <div>
+        <div className='pb-12 mb-8 bg-white rounded-lg shadow-lg lg:p-8'>
             <h1>Post Details</h1>
         </div>
     )
