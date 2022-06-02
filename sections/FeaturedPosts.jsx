81ac5f0070aef0705e9d0319315667dd37ff73f9
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-06-02 08:48:41 -0600

    arrow stuff

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -36,7 +36,7 @@ const FeaturedPosts = () => {
   }, []);
 
   const customLeftArrow = (
-    <div className="absolute left-0 py-3 text-center bg-pink-600 rounded-full cursor-pointer arrow-btn">
+    <div className="absolute left-0 py-3 text-center rounded-full cursor-pointer arrow-btn">
       <svg xmlns="http://www.w3.org/2000/svg" className="w-6 w-full h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
@@ -44,7 +44,7 @@ const FeaturedPosts = () => {
   );
 
   const customRightArrow = (
-    <div className="absolute right-0 py-3 text-center bg-pink-600 rounded-full cursor-pointer arrow-btn">
+    <div className="absolute right-0 py-3 cursor-pointer text-centerrounded-full arrow-btn">
       <svg xmlns="http://www.w3.org/2000/svg" className="w-6 w-full h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
       </svg>
