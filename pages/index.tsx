7c2b956c63cec985a50f2aac960255149d8ca778
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-06-01 23:46:49 -0600

    object type

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 
-export default function Home({ posts }) {
+export default function Home({ posts } : {posts: string}) {
   return (
     <div className="container px-10 mx-auto mb-8">
       <FeaturedPosts />
