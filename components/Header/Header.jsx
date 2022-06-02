7d6497f1836099cd48aed39d6ad8d218dab6e782
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-06-02 08:44:41 -0600

    font-size

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -26,8 +26,8 @@ const Header = () => {
             <div className='inline-block w-full py-8 border-b border-emerald-400'>
                 <div className='block md:float-left'>
                     <Link href='/'>
-                        <span className='text-4xl font-light font-bold text-white cursor-pointer font-sourceL'>
-                            <span className='font-light ext-4xl text-emerald-400'>C:\ </span> 
+                        <span className='text-3xl font-light text-white cursor-pointer font-sourceL'>
+                            <span className='text-3xl font-light text-emerald-400'>C:\ </span> 
                             <Typed
                     strings={['BATCH FORUM']}
                     typeSpeed={100}
